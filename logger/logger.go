27cac3794406ea30a561863e1b55accc55d675de
damon <d@petta.org>
2024-09-15 07:29:34 -0400

    fix: Update logger import path

--- logger/logger.go
+++ logger/logger.go
@@ -3,7 +3,7 @@ package logger
 
 import (
 	"github.com/rs/zerolog"
-	"github.com/traefik/traefik/v3/pkg/log"
+	"github.com/traefik/traefik/v3/pkg/logs"
 )
 
 // Logger is an interface that represents the logging capabilities required by the Forklift middleware.
